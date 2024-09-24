a4f0091459cc369475d6baf01b839bfd1634c79d
Sharlottaa <dasha.sidorenko1111@mail.ru>
2024-09-24 18:13:49 +0300

    - Убрала конструктор. Вся логика в сервисах и конструктор там. Реализованы исключения. Формулу использовала вместо switch case

--- src/main/java/tech/reliab/course/toropchinda/bank/Main.java
+++ src/main/java/tech/reliab/course/toropchinda/bank/Main.java
@@ -1,7 +1,6 @@
 package main.java.tech.reliab.course.toropchinda.bank;
 
 import main.java.tech.reliab.course.toropchinda.bank.entity.*;
-import main.java.tech.reliab.course.toropchinda.bank.service.*;
 import main.java.tech.reliab.course.toropchinda.bank.service.impl.*;
 
 import java.util.Date;
